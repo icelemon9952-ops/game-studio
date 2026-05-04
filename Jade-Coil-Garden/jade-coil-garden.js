50b2bc4c7c9d0fd58ab7ef81dc9d31098199e6ce
shelley9952 <icelemon9952@gmail.com>
2026-05-05 01:28:41 +0800

    Add Classic Snake JS loader

---
+++ Jade-Coil-Garden/jade-coil-garden.js
@@ -0,0 +1,4 @@
+window.__JCG_GAME_CHUNKS=[];
+document.write('<script src="./jade-coil-garden.chunk1.js"><\/script>');
+document.write('<script src="./jade-coil-garden.chunk2.js"><\/script>');
+document.write('<script src="./jade-coil-garden.run.js"><\/script>');
