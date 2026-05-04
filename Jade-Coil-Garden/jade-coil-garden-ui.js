4677c35fa84c1f9188a308c3c3291fef32f77acd
shelley9952 <icelemon9952@gmail.com>
2026-05-05 01:36:44 +0800

    Add Classic Snake UI JS loader

---
+++ Jade-Coil-Garden/jade-coil-garden-ui.js
@@ -0,0 +1,4 @@
+window.__JCG_UI_CHUNKS=[];
+document.write('<script src="./jade-coil-garden-ui.chunk1.js"><\/script>');
+document.write('<script src="./jade-coil-garden-ui.chunk2.js"><\/script>');
+document.write('<script src="./jade-coil-garden-ui.run.js"><\/script>');
